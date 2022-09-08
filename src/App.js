aae25b5185742e3f1cfec09a5d8f9517d5a1d8ef
ajadi yusuf <yusufajadi93@gmail.com>
2022-09-08 21:27:02 +0100

    updated delete method

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import './App.css';
 const axios = require('axios');
 
-// axios.post('https://jsonplaceholder.typicode.com/posts', {
-//   title: 'Cats',
-//   body: 'Cats are lovely',
-//   userId: 10,
-// })
-// .then(function (response) {
-//   console.log(response);
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
+//CREATE
+axios.post('https://jsonplaceholder.typicode.com/posts', {
+  title: 'Cats',
+  body: 'Cats are lovely',
+  userId: 10,
+})
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
 
+//READ
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-// axios.get('https://jsonplaceholder.typicode.com/posts')
-//   .then(function (response) {
-//     console.log(response.data);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-
+  //UPDATE
   axios.put('https://jsonplaceholder.typicode.com/posts/1', {
-  title: 'Lions',
-  body: 'Lions are not lovely',
-  userId: 1,
+    title: 'Lions',
+    body: 'Lions are not lovely',
+    userId: 1,
   })
   .then(function (response) {
     console.log(response);
@@ -34,18 +36,20 @@ const axios = require('axios');
     console.log(error);
   });
 
-//  axios.delete('https://jsonplaceholder.typicode.com/posts/1')
-//   .then(function (response) {
-//     console.log(response.data);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+  //DELETE
+ axios.delete('https://jsonplaceholder.typicode.com/posts/1')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
  
 function App() {
   return (
     <div className="App">
-       <h1>Hello</h1>      
+       <h1>Welcome</h1>
+       <p>Kindly open the developer console to view output.</p>      
     </div>
   );
 }
